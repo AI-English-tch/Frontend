f00ad30edfb1f60f988419f89752e651d3bc6a97
HQsen <2532953033@qq.com>
2023-05-31 22:14:32 +0800

    最新前端

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import installElementPlus from './plugins/element'
-import element from './plugins/element'
-const app = createApp(App)
-installElementPlus(app)
-app.use(router).use(element).mount('#app')
